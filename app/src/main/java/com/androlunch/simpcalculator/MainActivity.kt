26affb88d32ac41aa3975618b818771589632296
shitol-roy <shitolroy48.roy@gmail.com>
2023-08-24 20:00:37 +0600

    Android Calculator App made by Shitol Roy(Jetpack-kotlin)

---
+++ app/src/main/java/com/androlunch/simpcalculator/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.androlunch.simpcalculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.androlunch.simpcalculator.ui.theme.MediumGray
+import com.androlunch.simpcalculator.ui.theme.SimpCalculatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SimpCalculatorTheme {
+                val viewmodel = viewModel<CalculatorViewModel>()
+                val state = viewmodel.state
+                val buttonSpacing = 8.dp
+                Calculator(
+                    state = state,
+                    onAction = viewmodel::onAction ,
+                    buttonSpacing = buttonSpacing ,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MediumGray)
+                        .padding(16.dp)
+                )
+
+            }
+        }
+    }
+}
